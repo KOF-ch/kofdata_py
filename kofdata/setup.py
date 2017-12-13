a4363e26b2efa1814ad9bc308a96f80f1cb53335
thoenis <sthoeni@kof.ethz.ch>
2017-12-13 16:21:34 +0100

    Create package subdir and setup.py

---
+++ kofdata/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='kofdata',
+      version='0.1',
+      description='Python API Wrapper for the KOF Datenservice API',
+      url='https://github.com/KOF-ch/kofdata_py',
+      author='Severin Thoeni',
+      author_email='thoenis@kof.ethz.ch',
+      license='MIT',
+      packages=['kofdata'],
+      zip_safe=False)
