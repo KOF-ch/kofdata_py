00c97ca6dc38a94a957fe7fd04abd5cb71618940
thoenis <sthoeni@kof.ethz.ch>
2017-12-14 13:52:04 +0100

    Update setup.py, change author e-mail

--- kofdata/setup.py
+++ kofdata/setup.py
@@ -2,10 +2,10 @@ from setuptools import setup
 
 setup(name='kofdata',
       version='0.1',
-      description='Python API Wrapper for the KOF Datenservice API',
+      description='Python Wrapper for the KOF Datenservice API',
       url='https://github.com/KOF-ch/kofdata_py',
-      author='Severin Thoeni',
-      author_email='sthoeni@kof.ethz.ch',
+      author='Severin Thoeni, Matthias Bannert',
+      author_email='bannert@kof.ethz.ch',
       license='MIT',
       packages=['kofdata'],
       zip_safe=False)
